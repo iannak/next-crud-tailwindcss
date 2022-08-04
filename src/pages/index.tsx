63808daf440457c16fd5ee44d0e8e8689273515d
franzannakarolina <nerdstecnologia0@gmail.com>
2022-08-03 22:57:34 -0300

    iniciando projeto crud com next typescript tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600`}>
+      <span className="text-fuchsia-800 text-4xl">Home</span>
+    </div>
+  )
+}
